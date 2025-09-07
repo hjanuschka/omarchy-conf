246b8c12c45ff9d94f5659aa9aa089d2bad2343f
Helmut Januschka <hjanuschka@fastmail.com>
2025-09-07 08:36:35 +0200

    up

---
+++ .config/omarchy/themes/my-ristretto/apply-light-mode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Apply light mode settings for my-ristretto theme
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+echo "Light mode applied for my-ristretto theme"
